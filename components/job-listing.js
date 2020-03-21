ee16a3599655c249b5b99089944c1d8a70dbdf2e
cagloria <33134245+cagloria@users.noreply.github.com>
2020-03-20 21:23:16 -0700

    Added new/featured indicator and company logo

--- components/job-listing.js
+++ components/job-listing.js
@@ -15,16 +15,23 @@ class JobListing extends HTMLElement {
 
         let jobObj = JOBS.returnJobs().find(job => job.id === id);
 
+        let topText = `
+            ${jobObj.company}
+            ${jobObj.isNew ? " New!" : ""}
+            ${jobObj.isFeatured ? " Featured" : ""}
+        `;
+
+        let logoPath = this.getCompanyLogo(jobObj.company);
+
         let htmlText = `
-            <p>${jobObj.company}</p>
+            <img width="88" height="88" src="${logoPath}" />
+            <p>${topText}</p>
             <p>${jobObj.title}</p>
             <p>${jobObj.time} - ${jobObj.shift} - ${jobObj.location}</p>
             <ul>
                 <li>${role}</li>
                 <li>${level}</li>
         `;
-
-        // TODO: Return is new/featured
 
         let languagesStr = this.getDataList(languages, "languages");
         if (languagesStr !== undefined) {
@@ -39,7 +46,7 @@ class JobListing extends HTMLElement {
         this.innerHTML = htmlText + "</ul>";
     }
 
-    getDataList(arrString, className) {
+    getDataList(arrString) {
         if (arrString !== "null") {
             let html = ``;
             let arr = arrString.split(",");
@@ -50,6 +57,27 @@ class JobListing extends HTMLElement {
 
             return html;
         }
+    }
+
+    getCompanyLogo(name) {
+        let list = [
+            { company: "Account", logo: "../images/account.svg" },
+            { company: "Eyecam Co.", logo: "../images/eyecam-co.svg" },
+            { company: "FaceIt", logo: "../images/faceit.svg" },
+            { company: "Insure", logo: "../images/insure.svg" },
+            { company: "Loop Studios", logo: "../images/loop-studios.svg" },
+            { company: "Manage", logo: "../images/manage.svg" },
+            { company: "MyHome", logo: "../images/myhome.svg" },
+            { company: "Photosnap", logo: "../images/photosnap.svg" },
+            { company: "Shortly", logo: "../images/shortly.svg" },
+            {
+                company: "The Air Filter Company",
+                logo: "../images/the-air-filter-company.svg"
+            }
+        ];
+        let companyObj = list.find(company => company.company === name);
+
+        return companyObj.logo;
     }
 }
 
