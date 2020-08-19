965153a6f2dfd6a0b9606ec55330bcb0abe923a7
cagloria <33134245+cagloria@users.noreply.github.com>
2020-08-18 17:25:25 -0700

    Adjusted job and filter containers
    Adjusted margin and positioning

--- scripts/jobs.js
+++ scripts/jobs.js
@@ -216,7 +216,7 @@ function createJobListings() {
         $("#jobs-container").append(`
             <job-listing
                 id="job-${job.id}"
-                class="job-listing${featuredClass} content-row window-width"
+                class="job-listing${featuredClass} content-row"
                 data-role="${job.role}"
                 data-level="${job.level}"
                 data-languages="${job.languages}"
